b70b1eeb95d8e39c57d629bbba7f66d0abd966ae
Oiep <foive369@gmail.com>
2024-12-19 15:34:33 +0700

    Add files via upload

---
+++ source/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace kiemtra
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvListView = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMaSV = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtHoTen = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnTim = new System.Windows.Forms.Button();
+            this.txtTimKiem = new System.Windows.Forms.TextBox();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnLuu = new System.Windows.Forms.Button();
+            this.btnKhong = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.cbbLop = new System.Windows.Forms.ComboBox();
+            this.dtNgaySinh = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvListView
+            // 
+            this.dgvListView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvListView.Location = new System.Drawing.Point(46, 213);
+            this.dgvListView.Name = "dgvListView";
+            this.dgvListView.RowHeadersWidth = 51;
+            this.dgvListView.RowTemplate.Height = 24;
+            this.dgvListView.Size = new System.Drawing.Size(703, 257);
+            this.dgvListView.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(106, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Mã số sinh viên";
+            // 
+            // txtMaSV
+            // 
+            this.txtMaSV.Location = new System.Drawing.Point(211, 30);
+            this.txtMaSV.Name = "txtMaSV";
+            this.txtMaSV.Size = new System.Drawing.Size(100, 22);
+            this.txtMaSV.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(395, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Lớp học ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(381, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Họ tên ";
+            // 
+            // txtHoTen
+            // 
+            this.txtHoTen.Location = new System.Drawing.Point(436, 27);
+            this.txtHoTen.Name = "txtHoTen";
+            this.txtHoTen.Size = new System.Drawing.Size(128, 22);
+            this.txtHoTen.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(106, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 16);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Ngày sinh ";
+            // 
+            // btnTim
+            // 
+            this.btnTim.Location = new System.Drawing.Point(150, 130);
+            this.btnTim.Name = "btnTim";
+            this.btnTim.Size = new System.Drawing.Size(75, 47);
+            this.btnTim.TabIndex = 4;
+            this.btnTim.Text = "Tìm ";
+            this.btnTim.UseVisualStyleBackColor = true;
+            this.btnTim.Click += new System.EventHandler(this.btnTim_Click);
+            // 
+            // txtTimKiem
+            // 
+            this.txtTimKiem.Location = new System.Drawing.Point(12, 142);
+            this.txtTimKiem.Name = "txtTimKiem";
+            this.txtTimKiem.Size = new System.Drawing.Size(119, 22);
+            this.txtTimKiem.TabIndex = 5;
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(236, 136);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(75, 35);
+            this.btnThem.TabIndex = 6;
+            this.btnThem.Text = "Thêm ";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(317, 136);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(75, 35);
+            this.btnXoa.TabIndex = 6;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(398, 136);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(75, 35);
+            this.btnSua.TabIndex = 6;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.Location = new System.Drawing.Point(479, 136);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(75, 35);
+            this.btnLuu.TabIndex = 6;
+            this.btnLuu.Text = "Lưu ";
+            this.btnLuu.UseVisualStyleBackColor = true;
+            this.btnLuu.Click += new System.EventHandler(this.btnLuu_Click);
+            // 
+            // btnKhong
+            // 
+            this.btnKhong.Location = new System.Drawing.Point(560, 136);
+            this.btnKhong.Name = "btnKhong";
+            this.btnKhong.Size = new System.Drawing.Size(75, 35);
+            this.btnKhong.TabIndex = 6;
+            this.btnKhong.Text = "K.Lưu";
+            this.btnKhong.UseVisualStyleBackColor = true;
+            this.btnKhong.Click += new System.EventHandler(this.btnKhong_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(641, 136);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 35);
+            this.btnThoat.TabIndex = 6;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // cbbLop
+            // 
+            this.cbbLop.FormattingEnabled = true;
+            this.cbbLop.Location = new System.Drawing.Point(461, 74);
+            this.cbbLop.Name = "cbbLop";
+            this.cbbLop.Size = new System.Drawing.Size(143, 24);
+            this.cbbLop.TabIndex = 3;
+            // 
+            // dtNgaySinh
+            // 
+            this.dtNgaySinh.Location = new System.Drawing.Point(182, 76);
+            this.dtNgaySinh.Name = "dtNgaySinh";
+            this.dtNgaySinh.Size = new System.Drawing.Size(200, 22);
+            this.dtNgaySinh.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 504);
+            this.Controls.Add(this.dtNgaySinh);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnKhong);
+            this.Controls.Add(this.btnLuu);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.txtTimKiem);
+            this.Controls.Add(this.btnTim);
+            this.Controls.Add(this.cbbLop);
+            this.Controls.Add(this.txtHoTen);
+            this.Controls.Add(this.txtMaSV);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvListView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvListView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMaSV;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtHoTen;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnTim;
+        private System.Windows.Forms.TextBox txtTimKiem;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnLuu;
+        private System.Windows.Forms.Button btnKhong;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.ComboBox cbbLop;
+        private System.Windows.Forms.DateTimePicker dtNgaySinh;
+    }
+}
+
---
+++ source/Form1.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using kiemtra.Models;
+
+namespace kiemtra
+{
+    public partial class Form1 : Form
+    {
+        
+       ModelDbSinhVien DbSinhVien = new ModelDbSinhVien();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            setupDGVColumns();
+            fillDGVSinhVien();
+            fillCBBLop();
+       
+
+        }
+
+        private void setupDGVColumns()
+        {
+            dgvListView.Columns.Clear(); // Xóa tất cả cột trước (nếu có)
+            dgvListView.Columns.Add("MaSV", "Mã SV");
+            dgvListView.Columns.Add("HoTenSV", "Họ Tên");
+            dgvListView.Columns.Add("NgaySinh", "Ngày Sinh");
+            dgvListView.Columns.Add("MaLop", "Mã Lớp");
+        }
+
+        private void fillCBBLop()
+        {
+            List<Lop> ListLop = DbSinhVien.Lops.ToList();
+
+            cbbLop.DataSource = ListLop;
+            cbbLop.DisplayMember = "TenLop";
+            cbbLop.ValueMember = "MaLop";         
+
+
+        }
+
+        private void fillDGVSinhVien()
+        {
+            dgvListView.Rows.Clear();
+
+            List<Sinhvien> ListSinhVien = DbSinhVien.Sinhviens.ToList();
+
+            foreach (Sinhvien sinhvien in ListSinhVien)
+            {
+                int newRow = dgvListView.Rows.Add();
+
+                dgvListView.Rows[newRow].Cells[0].Value = sinhvien.MaSV;
+                dgvListView.Rows[newRow].Cells[1].Value = sinhvien.HoTenSV;
+                dgvListView.Rows[newRow].Cells[2].Value = sinhvien.NgaySinh;
+                dgvListView.Rows[newRow].Cells[3].Value = sinhvien.MaLop;
+
+            }
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            string maSV = txtMaSV.Text;
+            string hoTen = txtHoTen.Text;
+            DateTime ngaySinh = dtNgaySinh.Value;
+            string maLop = cbbLop.SelectedValue.ToString();
+
+            Sinhvien newSinhVien = new Sinhvien
+            {
+                MaSV = maSV,
+                HoTenSV = hoTen,
+                NgaySinh = ngaySinh,
+                MaLop = maLop
+            };
+
+            DbSinhVien.Sinhviens.Add(newSinhVien);
+            DbSinhVien.SaveChanges();
+
+            MessageBox.Show("Thêm sinh viên thành công!");
+            fillDGVSinhVien();
+            ClearInputFields();
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if (dgvListView.SelectedRows.Count > 0)
+            {
+                string maSV = dgvListView.SelectedRows[0].Cells[0].Value.ToString();
+                Sinhvien existingSinhVien = DbSinhVien.Sinhviens.FirstOrDefault(sv => sv.MaSV == maSV);
+
+
+                var confirmResult = MessageBox.Show("Bạn có chắc chắn muốn xóa không?", "Xác nhận thoát", MessageBoxButtons.YesNo);
+                if (confirmResult == DialogResult.Yes)
+                {
+
+
+                    if (existingSinhVien != null)
+                    {
+                        DbSinhVien.Sinhviens.Remove(existingSinhVien);
+                        DbSinhVien.SaveChanges();
+
+                        MessageBox.Show("Xóa sinh viên thành công!");
+                        fillDGVSinhVien();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Không tìm thấy sinh viên!");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Vui lòng chọn một sinh viên để xóa!");
+                }
+            }
+
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            if (dgvListView.SelectedRows.Count > 0)
+            {
+                string maSV = dgvListView.SelectedRows[0].Cells[0].Value.ToString();
+                Sinhvien existingSinhVien = DbSinhVien.Sinhviens.FirstOrDefault(sv => sv.MaSV == maSV);
+
+                if (existingSinhVien != null)
+                {
+                    existingSinhVien.HoTenSV = txtHoTen.Text;
+                    existingSinhVien.NgaySinh = dtNgaySinh.Value;
+                    existingSinhVien.MaLop = cbbLop.SelectedValue.ToString();
+
+                    DbSinhVien.SaveChanges();
+
+                    MessageBox.Show("Cập nhật sinh viên thành công!");
+                    fillDGVSinhVien();
+                }
+                else
+                {
+                    MessageBox.Show("Không tìm thấy sinh viên!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn một sinh viên để sửa!");
+            }
+            ClearInputFields();
+        }
+
+        //Hiệu ứng cho nút lưu và không lưu
+        private void setButtonState(bool isEditing)
+        {
+            btnThem.Enabled = !isEditing;
+            btnSua.Enabled = !isEditing;
+            btnXoa.Enabled = !isEditing;
+
+            btnLuu.Enabled = isEditing;
+            btnKhong.Enabled = isEditing;
+
+          
+            
+            //txtMaSV.Enabled = isEditing; 
+            //txtHoTen.Enabled = isEditing;
+            //dtNgaySinh.Enabled = isEditing;
+            //cbbLop.Enabled = isEditing;
+        }
+        
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            txtMaSV.Text = ""; // Xóa các ô nhập liệu
+            txtHoTen.Text = "";
+            dtNgaySinh.Value = DateTime.Now;
+            cbbLop.SelectedIndex = -1;
+
+            setButtonState(true); // Kích hoạt chế độ chỉnh sửa
+        }
+
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (dgvListView.SelectedRows.Count > 0)
+            {
+                setButtonState(true); // Kích hoạt chế độ chỉnh sửa
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn một sinh viên để sửa!");
+            }
+        }
+
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            string maSV = txtMaSV.Text;
+            string hoTen = txtHoTen.Text;
+            DateTime ngaySinh = dtNgaySinh.Value;
+            string maLop = cbbLop.SelectedValue?.ToString();
+
+            if (string.IsNullOrEmpty(maSV) || string.IsNullOrEmpty(hoTen) || string.IsNullOrEmpty(maLop))
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin!");
+                return;
+            }
+
+            // Kiểm tra xem đây là thêm mới hay chỉnh sửa
+            Sinhvien sinhvien = DbSinhVien.Sinhviens.FirstOrDefault(s => s.MaSV == maSV);
+            if (sinhvien == null)
+            {
+                // Thêm mới
+                sinhvien = new Sinhvien { MaSV = maSV };
+                DbSinhVien.Sinhviens.Add(sinhvien);
+            }
+
+            // Cập nhật thông tin
+            sinhvien.HoTenSV = hoTen;
+            sinhvien.NgaySinh = ngaySinh;
+            sinhvien.MaLop = maLop;
+
+            DbSinhVien.SaveChanges(); // Lưu vào cơ sở dữ liệu
+
+            fillDGVSinhVien(); // Làm mới DataGridView
+            setButtonState(false); // Tắt chế độ chỉnh sửa
+        }
+
+        private void btnKhong_Click(object sender, EventArgs e)
+        {
+            if (dgvListView.SelectedRows.Count > 0)
+            {
+                // Khôi phục dữ liệu từ hàng đang chọn
+                DataGridViewRow selectedRow = dgvListView.SelectedRows[0];
+                txtMaSV.Text = selectedRow.Cells[0].Value?.ToString();
+                txtHoTen.Text = selectedRow.Cells[1].Value?.ToString();
+                dtNgaySinh.Value = DateTime.Parse(selectedRow.Cells[2].Value?.ToString());
+                cbbLop.SelectedValue = selectedRow.Cells[3].Value?.ToString();
+            }
+            else
+            {
+                // Nếu không có hàng nào được chọn
+                txtMaSV.Text = "";
+                txtHoTen.Text = "";
+                dtNgaySinh.Value = DateTime.Now;
+                cbbLop.SelectedIndex = -1;
+            }
+
+            setButtonState(false); // Tắt chế độ chỉnh sửa
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+           // Show a message box to confirm exit
+        var confirmResult = MessageBox.Show("Bạn có chắc chắn muốn thoát ứng dụng?", "Xác nhận thoát", MessageBoxButtons.YesNo);
+            if (confirmResult == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+
+        }
+
+        private void btnTim_Click(object sender, EventArgs e)
+        {
+            string searchName = txtTimKiem.Text.ToLower().Trim();
+
+            List<Sinhvien> filteredList = DbSinhVien.Sinhviens
+                .Where(sv => sv.HoTenSV.ToLower().Contains(searchName))
+                .ToList();
+
+            dgvListView.Rows.Clear();
+
+            foreach (Sinhvien sinhvien in filteredList)
+            {
+                int newRow = dgvListView.Rows.Add();
+                dgvListView.Rows[newRow].Cells[0].Value = sinhvien.MaSV;
+                dgvListView.Rows[newRow].Cells[1].Value = sinhvien.HoTenSV;
+                dgvListView.Rows[newRow].Cells[2].Value = sinhvien.NgaySinh;
+                dgvListView.Rows[newRow].Cells[3].Value = sinhvien.MaLop;
+            }
+
+            if (filteredList.Count == 0)
+            {
+                MessageBox.Show("Không tìm thấy sinh viên nào với tên '" + searchName + "'.");
+            }
+        }
+        private void ClearInputFields()
+        {
+            txtMaSV.Text = "";
+            txtHoTen.Text = "";
+            dtNgaySinh.Value = DateTime.Now;
+            cbbLop.SelectedIndex = -1;
+        }
+    }
+}
